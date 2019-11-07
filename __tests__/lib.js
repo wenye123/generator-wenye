8e73652fe16f788d46b4fbe7728d1d9faffe15e9
wenye <1583247975@qq.com>
2019-11-07 22:47:27 +0800

    feat(lib): 增加wenye:lib模板

---
+++ __tests__/lib.js
@@ -0,0 +1,27 @@
+"use strict";
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
+
+describe("generator-wenye:lib", () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, "../generators/lib")).withPrompts({
+      name: "wenye-hello",
+      version: "1.0.0",
+      description: "hello world",
+      keywords: "hello,wenye",
+      author: "wenye",
+      license: "MIT"
+    });
+  });
+
+  it("creates files", () => {
+    assert.file(["README.md"]);
+    assert.file(["tsconfig.json"]);
+    assert.file([".gitignore"]);
+    assert.file([".editorconfig"]);
+    assert.file([".prettierrc.js"]);
+    assert.file(["src"]);
+    assert.file(["test"]);
+  });
+});
---
+++ generators/lib/templates/test/test-index.ts
@@ -0,0 +1,8 @@
+import { str } from "../src/index";
+import assert from "assert";
+
+describe("test", function() {
+  it("hello", function() {
+    assert.equal(str, "hello");
+  });
+});
